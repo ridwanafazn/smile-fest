23e3a6fd1bc563690c9933830449113acd4b67a6
Ridwan Ahmad Fauzan <ridwanafzn@gmail.com>
2026-05-20 09:16:15 +0700

    change - fix api to cloud run

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://smile-fest-api.up.railway.app';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://smile-fest-api-143705141615.asia-southeast2.run.app/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
